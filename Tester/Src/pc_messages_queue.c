b6ca5eb27ab2f49745ad7f7b04a8d03b26a8f956
RKulagin <rus.kulagin2001@yandex.ru>
2022-12-09 12:37:37 +0300

    Q

--- Tester/Src/pc_messages_queue.c
+++ Tester/Src/pc_messages_queue.c
@@ -1,16 +1,20 @@
 #include "pc_messages_queue.h"
 
 
-Queue* PC_Messages_Queue_Init(){
-	// Malloc
+struct Queue* PC_Messages_Queue_Init(){
+	struct Queue* queue = malloc(sizeof(struct Queue));
+	if (queue == NULL){
+		return NULL;
+	}
+	return queue;
 }
 
-void Push(Queue* q, uint_8* message){
+void Push(struct Queue* q, uint_8* message){
 	strcpy(q->queue[q->end], message);
 	q->end = (q->end + 1) % PC_MESSAGES_QUEUE_SIZE;
 }
 
-uint_8* Pop(Queue* q){
+uint_8* Pop(struct Queue* q){
 	uint_8* poped = q->queue[q->start];
 	q->queue[q->start] = NULL;
 	q->start = (q->end + 1) % PC_MESSAGES_QUEUE_SIZE;
